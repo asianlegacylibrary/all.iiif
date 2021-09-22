05f8a47dbdff8111beeb076aa2f6d58911280a80
joelcrawford <joelrootcrawford@gmail.com>
2021-09-21 17:03:03 -0700

    updated production URL to netlify and ALL

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import React from 'react'
 import logo from './assets/img/logo512.png'
 import './assets/css/App.css'
 
-const manifestLink = (
-    <a
-        className="manifest-link"
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://iiif.asianlegacylibrary.org/uv.html?manifest=http://iiifpres.bdrc.io/2.1.1/v:bdr:V1NLM1957_I1NLM1957_001/manifest"
-    >
-        :: ACIP hosted IIIF ::
-    </a>
-)
+const manifestURLS = [
+    {
+        key: 'Asian Legacy Library',
+        url:
+            'https://all-library-manifests.s3.us-west-2.amazonaws.com/ISKS1RC8012.IG.ISKS1RC8012.001.manifest.json',
+    },
+    {
+        key: 'BDRC',
+        url:
+            'http://iiifpres.bdrc.io/2.1.1/v:bdr:V1NLM1957_I1NLM1957_001/manifest',
+    },
+]
+let mLinks = []
+manifestURLS.forEach((m) => {
+    let url = `https://iiif.asianlegacylibrary.org/uv.html?manifest=${m.url}`
+    mLinks.push(
+        <a
+            className="manifest-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={url}
+        >
+            :: {m.key} hosted IIIF ::
+        </a>
+    )
+})
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                {manifestLink}
+                {mLinks}
             </header>
         </div>
     )
